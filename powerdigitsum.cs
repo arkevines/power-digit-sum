e352ccdbf1850fe548aae1b1de167e2febd39e78
arkevines <arkadiuszwilczewski@gmail.com>
2015-04-20 11:39:19 +0200

    Update powerdigitsum.cs

--- powerdigitsum.cs
+++ powerdigitsum.cs
@@ -8,34 +8,34 @@ class mainclass
     {
         int sum = 0;
          
-        int[] arr = new int[400];
+        int[] digits_arr = new int[400];
         //losowa liczba, ktora zmieci wszystkie cyfry dzialania 2 ^ 1000 
-        arr[0] = 2;
+        digits_arr[0] = 2;
         int carry = 0;
  
         for (int j = 1; j < 1000; j++)
         {
-            for (int i = 0; i < arr.Length; i++)
+            for (int i = 0; i < digits_arr.Length; i++)
             {
-                int tempno = arr[i] * 2;
+                int number = digits_arr[i] * 2;
                  
-                if (tempno > 9)
+                if (number > 9)
                 {
-                    arr[i] = (tempno % 10) + carry;                    
+                    digits_arr[i] = (number % 10) + carry;                    
                     carry = 1;
                 }
                 else
                 {
-                    arr[i] = tempno + carry;                    
+                    digits_arr[i] = number + carry;                    
                     carry = 0;
                 }
             }
         }        
  
-        for (int i = 0; i < arr.Length; i++)        
+        for (int i = 0; i < digits_arr.Length; i++)        
         {
-            sum += arr[i];            
-            Console.Write(arr[i]);
+            sum += digits_arr[i];            
+            Console.Write(digits_arr[i]);
         }
  
         Console.WriteLine();
